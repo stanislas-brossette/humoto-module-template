42cd0ac0a4d0ae6d016e8f664f6a39db8e4ee1bb
stanislas-brossette <stanislas.brossette@gmail.com>
2017-10-13 16:07:10 +0200

    Develop all the necessary functions for the constraint approximation task. Still need to setup and run the optim of the approx cstr

--- extra_modules/example/include/tools/python/kinematic_constraint.py
+++ extra_modules/example/include/tools/python/kinematic_constraint.py
@@ -17,16 +17,29 @@ l1 = 0.5
 l2 = 0.5
 
 def main():
-    xHip = np.array([0.4, 0.0])
+    xHip = np.array([0.7, 0.1])
     print('xHip target:', xHip)
-    thetaZero = np.array([0.1, 0.2])
-    test_finite_diff(thetaZero[0], thetaZero[1])
-    resIK = inverse_kinematics(xHip, thetaZero)
-    print('resIK:', resIK)
-    foundXHip = direct_kinematics(resIK[0], resIK[1])
-    print('xHip result:', foundXHip)
+    #thetaZero = np.array([0.1, 0.2])
+    #test_finite_diff(thetaZero[0], thetaZero[1])
+    #resIK = inverse_kinematics(xHip, thetaZero)
+    #print('resIK:', resIK)
+    #foundXHip = direct_kinematics(resIK[0], resIK[1])
+    #print('xHip result:', foundXHip)
+    #plot(resIK,xHip)
     #plot()
+    kneeFromHip(xHip)
 
+#This function returns the nonlinear constraint that we want to approximate
+def kin_cstr(xHip, xKneeMin=np.array([0.3,0.1]), xKneeMax=np.array([0.5,0.4])):
+    knee = kneeFromHip(xHip)
+    res = np.zeros(4)
+    res[0] = xKneeMax[0]-knee[0]
+    res[1] = xKneeMax[1]-knee[1]
+    res[2] = -xKneeMin[0]+knee[0]
+    res[3] = -xKneeMin[1]+knee[1]
+    return res
+
+#This function is used to check the derivatives by finite difference
 def test_finite_diff(theta1, theta2):
     delta = 1e-8
     xHip = np.array([0.5,0.1])
@@ -50,6 +63,7 @@ def test_finite_diff(theta1, theta2):
     dfdthetaCalc = cost_func_deriv(theta, xHip)
     print('dfdthetaCalc', dfdthetaCalc, '\n')
 
+#Computes the direct kinematics from theta (returns knee and hip positions)
 def direct_kinematics(theta1, theta2):
     x_knee = np.array([l1*np.cos(theta1), l1*np.sin(theta1)])
     x_hip = x_knee + np.array([l2*np.cos(theta1 + theta2), l2*np.sin(theta1 + theta2)])
@@ -57,6 +71,7 @@ def direct_kinematics(theta1, theta2):
     res = robotKinematics(x_knee, x_hip)
     return res
 
+#Computes the jacobians of the knee and hip positions
 def jacobian(theta1, theta2):
     jac_hip = np.matrix([[-l1*np.sin(theta1) - l2*np.sin(theta1 + theta2), -l2*np.sin(theta1 + theta2)],
                          [+l1*np.cos(theta1) + l2*np.cos(theta1 + theta2), +l2*np.cos(theta1 + theta2) ]])
@@ -66,11 +81,13 @@ def jacobian(theta1, theta2):
     jac = robotJacobians(jac_knee, jac_hip)
     return jac
 
+#Cost function measuring the squared distance to the target hip position
 def cost_func(theta, xHip):
     kin = direct_kinematics(theta[0], theta[1])
     res = (kin.hip[0]-xHip[0])**2 + (kin.hip[1]-xHip[1])**2
     return res
 
+#Derivatives of the cost function
 def cost_func_deriv(theta, xHip):
     kin = direct_kinematics(theta[0], theta[1])
     jac = jacobian(theta[0], theta[1])
@@ -79,12 +96,15 @@ def cost_func_deriv(theta, xHip):
     return np.array([dfdtheta0, dfdtheta1])
 
     
-def inverse_kinematics(xHip, thetaInit):
+#Computes thetas to reach the xHip goal with a simple minimization. Bounds cstr could be added
+def inverse_kinematics(xHip, thetaInit=np.array([0.13,-0.15])):
     angles = collections.namedtuple('angles', ['theta1', 'theta2'])
     bnds = ((-1.0, 1.0),(-2.0, 0.0))
-    res = minimize(cost_func, thetaInit, args=(xHip), jac=cost_func_deriv, bounds=bnds, method='SLSQP', options={'disp':True})
+    res = minimize(cost_func, thetaInit, args=(xHip), jac=cost_func_deriv, method='SLSQP', options={'disp':True})
+    #res = minimize(cost_func, thetaInit, args=(xHip), jac=cost_func_deriv, bounds=bnds, method='SLSQP', options={'disp':True})
     return res.x
 
+#Funny plot of trajectories of the robot
 def plot():
     nPoints = 1000
     vTheta1 = np.linspace(0,2*np.pi,nPoints)
@@ -103,5 +123,21 @@ def plot():
     plt.plot(vx_hip, vy_hip)
     plt.show()
 
+#Plots the robot and target
+def plot(theta, xHipGoal):
+    kin = direct_kinematics(theta[0], theta[1])
+    x = np.array([0.0, kin.knee[0], kin.hip[0]])
+    y = np.array([0.0, kin.knee[1], kin.hip[1]])
+    plt.plot(x, y)
+    plt.plot(xHipGoal[0], xHipGoal[1], 'rx')
+    plt.show()
+
+#Returns the location of the knee for a given hip position
+def kneeFromHip(xHip):
+    theta = inverse_kinematics(xHip)
+    kin = direct_kinematics(theta[0], theta[1])
+    print('Knee:', kin.knee)
+    plot(theta,xHip)
+    return kin.knee
 
 main()
